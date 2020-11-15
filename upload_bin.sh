cd1cb5497da50d46e9b609a4cce3600bf2e6af71
cxzzzz <cxz19961010@163.com>
2020-11-15 14:08:55 +0800

    由于服务器地址更换，更新upload脚本

--- upload_bin.sh
+++ upload_bin.sh
@@ -1,2 +1,2 @@
-scp  gpio_tests.bin  xingzhou@192.168.226.156:/home/xingzhou/programming/s2chip_verilog_sim/s2chip_hw/src/cm3_iot/m3designstart/logical/testbench/testcodes/s2chip/s2chip.bin
-scp  
+scp  gpio_tests.bin  xingzhou@10.130.147.228:/home/xingzhou/programming/s2chip_verilog_sim/s2chip_hw/contrib/sim_vcs/net/s2chip.bin
+#scp  gpio_tests.bin  xingzhou@10.130.147.228:/home/xingzhou/programming/s2chip_verilog_sim/s2chip_hw/src/cm3_iot/m3designstart/logical/testbench/testcodes/s2chip/s2chip.bin
